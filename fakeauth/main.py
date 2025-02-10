7a274dcd97f9249bae20794e819fbd62ad118ff4
Andreas Hammer Håversen <andreas.haversen@bekk.no>
2025-02-10 12:46:33 +0100

    fix keys

--- fakeauth/main.py
+++ fakeauth/main.py
@@ -158,4 +158,4 @@ key = get_or_create_jwk()
 
 @app.get("/discovery/v2.0/keys")
 def jwks():
-    return {"keys": [AUTH_CLIENT_JWKS["keys"], key]}
+    return {"keys": [AUTH_CLIENT_JWKS["keys"][0], key]}

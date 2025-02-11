d1e6597117697015da96c1d8bb32a2eab8edd848
Andreas Hammer Håversen <andreas.haversen@bekk.no>
2025-02-11 15:43:23 +0100

    dont use kubernetes to read secrets in someapp

--- some_app/main.py
+++ some_app/main.py
@@ -1,32 +1,25 @@
 from fastapi import FastAPI, Request
 from jwcrypto import jwk, jwt
 from datetime import datetime, timedelta
-from kubernetes import client, config
 from base64 import b64decode
-import json
 import requests
 import uuid
 import os
 
 app = FastAPI()
 
-config.load_incluster_config()
-
-v1 = client.CoreV1Api()
-
 
 def read_secret(secrets, name):
     return b64decode(secrets.data[name]).decode()
 
 
-def create_client_assertion(secrets):
+def create_client_assertion():
     # id registered in tokendings
-    CLIENT_ID = read_secret(secrets, "TOKEN_X_CLIENT_ID")
+    CLIENT_ID = os.getenv("TOKEN_X_CLIENT_ID")
     # jwk_key = key registered in tokendings
-    JWK_KEY = json.loads(read_secret(secrets, "TOKEN_X_PRIVATE_JWK"))
-    TOKEN_ENDPOINT = read_secret(secrets, "TOKEN_X_TOKEN_ENDPOINT")
+    key_string = os.getenv("TOKEN_X_PRIVATE_JWK")
+    TOKEN_ENDPOINT = os.getenv("TOKEN_X_TOKEN_ENDPOINT")
 
-    key_string = json.dumps(JWK_KEY)
     client_jwks = jwk.JWK.from_json(key_string)
     claims = {
         "sub": CLIENT_ID,  # who am i
@@ -44,12 +37,9 @@ def create_client_assertion(secrets):
 
 @app.get("/test/token/{aud}/{token}")
 def request_token(aud: str, token: str):
-    secret_from_env = os.getenv("JWKER_SECRET")
-    print(secret_from_env)
-    secrets = v1.read_namespaced_secret("some-app", "obo")
-    client_assertion_token = create_client_assertion(secrets)
+    client_assertion_token = create_client_assertion()
 
-    TOKEN_ENDPOINT = read_secret(secrets, "TOKEN_X_TOKEN_ENDPOINT")
+    TOKEN_ENDPOINT = os.getenv("TOKEN_X_TOKEN_ENDPOINT") or ""
 
     payload = {
         "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",

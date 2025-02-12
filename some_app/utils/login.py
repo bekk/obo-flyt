37ebd7bb0aa9fff560387c4d2dc3daa8f39d4a39
Andreas Hammer Håversen <andreas.haversen@bekk.no>
2025-02-12 16:17:32 +0100

    simplified fake auth url

--- some_app/utils/login.py
+++ some_app/utils/login.py
@@ -6,7 +6,7 @@ FAKE_AUTH_URL = os.getenv("FAKE_AUTH_LOGIN_URL") or ""
 
 
 def login_with_fake_auth(audience: str) -> jwt.JWT:
-    req_url = os.path.join(FAKE_AUTH_URL, audience)
+    req_url = f"{FAKE_AUTH_URL}/{audience}"
     response = requests.get(req_url)
     if response.status_code != 200:
         raise Exception(response.content)
